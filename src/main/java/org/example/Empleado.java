fa59c8eaaabc9e37a252c1a17b26ff33131aacd1
ignaciosiva <ignsilva2023@udec.cl>
2025-05-14 21:18:45 -0400

    Empleado javadoc y toString

--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -1,68 +1,139 @@
 package org.example;
 
-public class Empleado implements Invitable{
+/**
+ * Clase que representa a empleado
+ */
+public class Empleado implements Invitable {
     private String id;
     private String apellidos;
     private String nombre;
     private String correo;
     private String departamento;
 
-    public Empleado(String id, String apellidos, String nombre, String correo){
+    /**
+     * Crea un nuevo empleado.
+     *
+     * @param id        Identificador del empleado.
+     * @param apellidos Apellidos del empleado.
+     * @param nombre    Nombre del empleado.
+     * @param correo    Correo electrónico del empleado.
+     */
+    public Empleado(String id, String apellidos, String nombre, String correo) {
         this.id = id;
         this.apellidos = apellidos;
         this.nombre = nombre;
         this.correo = correo;
     }
 
+    /**
+     * Envía una invitación al empleado para una reunión.
+     *
+     * @param reunion Reunion a la cual esta invitado el empleado.
+     */
     @Override
-    public void invitar(Reunion reunion){
-        String mensaje = "Estimado " + nombre + " " + apellidos + ", " + "está invitado a una reunion del tipo";
+    public void invitar(Reunion reunion) {
+        String mensaje = "Estimado " + nombre + " " + apellidos + ", está invitado a una reunión del tipo ";
 
-        if (reunion instanceof ReunionVirtual){
-            mensaje = mensaje + "virtual en el siguiente enlace: " + ((ReunionVirtual) reunion).getEnlace();
+        if (reunion instanceof ReunionVirtual) {
+            mensaje += "virtual en el siguiente enlace: " + ((ReunionVirtual) reunion).getEnlace();
         } else if (reunion instanceof ReunionPresencial) {
-            mensaje = mensaje + "presencial en la sala: " + ((ReunionPresencial) reunion).getSala();
+            mensaje += "presencial en la sala: " + ((ReunionPresencial) reunion).getSala();
         }
-        System.out.println("Invitacion enviada a " + nombre + "via email: " + correo + "con detalles: " + mensaje);
+
+        System.out.println("Invitación enviada a " + nombre + " vía email: " + correo + " con detalles: " + mensaje);
     }
 
-    //setters
-    public String getId(){
+    /**
+     * Obtiene el ID del empleado.
+     *
+     * @return ID del empleado.
+     */
+    public String getId() {
         return id;
     }
 
-    public String getApellidos(){
-        return apellidos;
-    }
-
-    public String getNombre(){
-        return nombre;
-    }
-
-    public String getCorreo(){
-        return correo;
-    }
-
-    public void setDepartamento(String departamento){
-        this.departamento = departamento;
-    }
-
-    //setters
-    public void setId(String id){
+    /**
+     * Establece el ID del empleado.
+     *
+     * @param id Nuevo ID.
+     */
+    public void setId(String id) {
         this.id = id;
     }
 
-    public void setApellidos(String apellidos){
+    /**
+     * Obtiene los apellidos del empleado.
+     *
+     * @return Apellidos del empleado.
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * Establece los apellidos del empleado.
+     *
+     * @param apellidos Nuevos apellidos.
+     */
+    public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
-    public void setNombre(String nombre){
+    /**
+     * Obtiene el nombre del empleado.
+     *
+     * @return Nombre del empleado.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Establece el nombre del empleado.
+     *
+     * @param nombre Nuevo nombre.
+     */
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public void setCorreo(String correo){
+    /**
+     * Obtiene el correo electrónico del empleado.
+     *
+     * @return Correo electrónico.
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * Establece el correo electrónico del empleado.
+     *
+     * @param correo Nuevo correo electrónico.
+     */
+    public void setCorreo(String correo) {
         this.correo = correo;
     }
 
+    /**
+     * Establece el departamento del empleado.
+     *
+     * @param departamento Nombre del departamento.
+     */
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    /**
+     *  Representacion escrita del empleado.
+     *
+     * @return Cadena con los datos del empleado.
+     */
+    @Override
+    public String toString() {
+        return "Empleado {\n" +
+                "  id = " + id + "\n" + "  apellidos = " + apellidos + "\n" + "  nombre = " + nombre + "\n" + "  correo = " + correo + "\n" + "  departamento = " + departamento + "\n" + "}";
+    }
 
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ import static java.time.temporal.ChronoUnit.SECONDS;
 public class Main {
     public static void main(String[] args) {
         Empleado e1 = new Empleado("123", "Salgado Espinoza", "Agustín", "agsalgado2023@udec.cl");
+        System.out.println(e1);
         Asistencia a1 = new Asistencia(e1, true, Instant.now());
         ReunionPresencial reunion = new ReunionPresencial(new Date(), Instant.now(), Duration.of(10,  SECONDS), e1, tipoReunion.MARKETING, "Sala 2023");
         System.out.println(reunion.getFecha());
